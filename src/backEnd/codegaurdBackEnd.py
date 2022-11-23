4242d580a2076095d1e341ede9efc275e8abcc3e
Ori Ashkenazi <oriash94@gmail.com>
2022-11-23 18:29:07 +0200

    add initial routing to Public DB

--- src/backEnd/codegaurdBackEnd.py
+++ src/backEnd/codegaurdBackEnd.py
@@ -1,7 +1,30 @@
 from flask import Flask
+import requests
+
+
 app = Flask(__name__)
 
 
 @app.route('/login', methods=['GET'])
 def login():
     return "this is working"
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    x = requests.get('https://services.nvd.nist.gov/rest/json/cves/2.0')
+    print(x.status_code)
+    if x.status_code == 200:
+            return x.json()
+    else:            
+     return "Get Request Failed!!."
+
+
+@app.route('/keyword', methods=['GET'])
+def search():
+    x = requests.get('https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch='+requests.args.get("keywordSearch"))
+    print(x.status_code)
+    if x.status_code == 200:
+            return x.json()
+    else:            
+     return "Get Request with parameter Failed!!."
