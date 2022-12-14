0cbc0d7e2afa9c0145e0ca24386b3baed7414b83
naor.yekutiely@gamil.com <naor.yekutiely@gmail.com>
2022-12-14 18:12:37 +0200

    removed new line

--- src/backEnd/server.py
+++ src/backEnd/server.py
@@ -2,11 +2,24 @@ from flask import Flask
 from flask import request, Response
 import json
 from dependencyExecutor import DependencyExecutor
+from leader_election_algo import LeaderElection
+import signal
+
+
+def handler(signum, frame):
+    le.zk.stop()
+    le.zk.close()
+    exit(1)
 
 
 app = Flask(__name__)
 dependencyExecutor = DependencyExecutor()
 fetchFromSource = dependencyExecutor.fetchFromSource
+
+app.secret_key = "super secret key"
+le = LeaderElection('localhost:2181', 'AppChat', '/election')
+le.register()
+signal.signal(signal.SIGINT, handler)
 
 
 @app.route('/scan', methods=['POST'])
