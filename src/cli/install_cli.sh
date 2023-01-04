7feb089270afab8ec0f4a6b30280a8ffd325dd57
naor.yekutiely@gamil.com <naor.yekutiely@gmail.com>
2023-01-04 17:48:29 +0200

    Fixed git path in install.cli

--- src/cli/install_cli.sh
+++ src/cli/install_cli.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+git_path=$1
 cd /usr/local/bin
 rm -f codeguard
-cp ~/Git/codeguard/src/cli/codeguard.sh /usr/local/bin
+cp $git_path/codeguard/src/cli/codeguard.sh /usr/local/bin
 mv ./codeguard.sh codeguard
 chmod +x codeguard
 
