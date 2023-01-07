6b71c653d1f0b4d2f7ff51851f90bc2f927678e5
saarelli123 <saarelias@saars-MacBook-Pro-2.local>
2023-01-07 10:31:47 +0200

    name change

--- src/backEnd/mongoDB.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-
-class MongoDBConnector():
-
-    def __init__(self):
-        conn_str = 'mongodb://localhost:27017'
-        client = MongoClient(conn_str, serverSelectionTimeoutMS=5000)
