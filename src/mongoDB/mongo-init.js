38e9b08231a3a4faf1849aa4d6e87d919d7bb616
saarelli123 <saarelias@saars-MacBook-Pro-2.local>
2023-01-08 22:53:38 +0200

    mongo docker config

--- src/mongoDB/mongo-init.js
+++ src/mongoDB/mongo-init.js
@@ -1,4 +1,5 @@
-db.createCollection("codegard_cache");
+rs.initiate( { _id: "rs0", members: [ { _id : 0, host : "mongodb1:27017" }, { _id : 1, host : "mongodb2:27017" }, { _id : 2, host : "mongodb3:27017" } ] } )
+db.codegard.createCollection("codegard_cache");
 db.codegard_cache.insertMany([
     {
         "name": "matplotlib",
